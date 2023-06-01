894ebe83959f43c6187fe22d04efaddbf85719a1
Edd7711 <edoardo.pierdiluca@gmail.com>
2023-06-02 01:24:58 +0200

    inizio implementazione cliente - acquistaController

---
+++ src/main/java/com/controller/cliente/AcquistaController.java
@@ -0,0 +1,67 @@
+package com.controller.cliente;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class AcquistaController {
+
+    @FXML
+    private Text abbonamentoAttivoLabel;
+
+    @FXML
+    private Button abbonamentoButton;
+
+    @FXML
+    private DatePicker abbonamentoDatePicker;
+
+    @FXML
+    private Button acquistaBigliettoButton;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private DatePicker bigliettoDatePicker;
+
+    @FXML
+    private Button cercaBigliettoButton;
+
+    @FXML
+    private ComboBox<?> filmComboBox;
+
+    @FXML
+    private ListView<?> filmList;
+
+    @FXML
+    private TextField postiTextField;
+
+    @FXML
+    private TextField quantitaPostiField;
+
+    @FXML
+    void acquistaBiglietto(MouseEvent event) {
+
+    }
+
+    @FXML
+    void back(MouseEvent event) {
+
+    }
+
+    @FXML
+    void cercaBiglietto(MouseEvent event) {
+
+    }
+
+    @FXML
+    void creaAbbonamento(MouseEvent event) {
+
+    }
+
+}
